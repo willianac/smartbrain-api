f58d3471ea541709bc7f1d8db6a3e569d203f35c
willianac <willianac@live.com>
2022-09-27 21:38:15 -0300

    fixing database

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      user : 'postgres',
-      password : 'test',
-      database : 'smartbraindb'
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   }); 
 
