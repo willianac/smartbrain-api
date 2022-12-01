ee296427f734f6195edf62772f4cda9cc7cc0fca
Willian Almeida <105891930+willianac@users.noreply.github.com>
2022-11-30 21:10:58 -0300

    fix

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(cors())
 const db = knex({
   client: 'pg',
   connection : {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    host : '127.0.0.1',
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'your_database'
   }
 })
 
